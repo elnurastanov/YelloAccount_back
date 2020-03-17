1e46496322a4e24d9a668088f2649fa4c954fd30
logmanCrater <44110804+logmanCrater@users.noreply.github.com>
2020-03-17 18:25:51 +0400

    salary panel connection to DB completed

--- src/routes/salary/salaryRouter.js
+++ src/routes/salary/salaryRouter.js
@@ -7,22 +7,24 @@ const route = () => {
     const router = new express.Router();
 
     router.route('/salary').post((req, res) => {
-        res.send(req.body)
         console.log(req.body);
-        
-        // 
-        DBconnect.query(`INSERT INTO salary_payment (sp_executing_company, sp_payment_channel, sp_payment_date, sp_partner, sp_destination, sp_vendor, sp_expense_type, sp_month_salary, sp_paid) VALUES ("${req.body.Executing_companyForm}" , "${req.body.Payment_channelForm}", "${req.body.formDate}", "${req.body.PartnerForm}", "${req.body.DestinationForm}", "${req.body.VendorForm}", "${req.body.expense_typeForm}", "${req.body.monthSalary}", "${req.body.paidSalary}")`,
+
+        DBconnect.query(`INSERT INTO salary_payment (sp_executing_company, sp_payment_channel, sp_payment_date, sp_partner, sp_destination, sp_vendor, sp_expense_type, sp_month_salary, sp_paid, sp_result, sp_result_percent) VALUES ("${req.body.Executing_companyForm}" , "${req.body.Payment_channelForm}", "${req.body.formDate}", "${req.body.PartnerForm}", "${req.body.DestinationForm}", "${req.body.VendorForm}", "${req.body.expense_typeForm}", "${req.body.monthSalary}", "${req.body.paidSalary}", "${req.body.monthSalary - req.body.paidSalary}", "${req.body.paidSalary / req.body.monthSalary * 100}")`,
             (err, result) => {
-            if (err) throw err;
-            console.log(result, "1 record inserted");
-            })
+                if (err) throw err;
+                console.log(result,"1 record inserted");
+                res.status(200).send((result.insertId).toString());
+        })
+
+        
+
     })
 
-        return router;
+    return router;
 
 }
 
 export default {
-        route,
-        routePrefix: `/${config.version}/panel`
+    route,
+    routePrefix: `/${config.version}/panel`
 }
